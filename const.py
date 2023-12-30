e47e809a61452ed17ed780f291dc8373039d7faa
jae-heo <gjwodud119@gmail.com>
2023-12-31 01:34:08 +0900

    Update: 운영체제 복붙 호환성 추가

--- const.py
+++ const.py
@@ -1,9 +1,15 @@
 from selenium.webdriver import Keys
+import platform
 
 BLOG_MAIN_URL = "https://section.blog.naver.com"
 NAVER_LOGIN_URL = "https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/"
 DEV_ID = "fuhafuha9"
 DEV_PW = "tofhdl19!"
 
-PASTE_KEY = Keys.COMMAND + 'v'
+if platform.system() == "Windows":
+    PASTE_KEY = Keys.LEFT_CONTROL + 'v'
+elif platform.system() == "Darwin":
+    PASTE_KEY = Keys.COMMAND + 'v'
+
+
 
