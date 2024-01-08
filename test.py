51d9efa0c08478a8ba97217dc9735f9bf141c96d
jae-heo <gjwodud119@gmail.com>
2024-01-08 23:14:03 +0900

    재영 테스트 추가

--- test.py
+++ test.py
@@ -5,6 +5,4 @@ from logic import *
 driver = get_chrome_driver()
 naver_login(driver, DEV_ID, DEV_PW)
 
-
-# get_blogs_by_search(driver, "롤 고수")
-get_blogs_by_category(driver, "생활·노하우·쇼핑", "반려동물")
+initialize(driver, DEV_ID)
